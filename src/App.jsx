a3a11f9626b573d9edff946d23ac6e4ccd8d0ec1
Zeynab Movasaqpoor <zeynab.mvs@gmail.com>
2024-03-18 20:58:51 +0330

    Implement theme toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,43 @@
-import { useState } from "react";
-import Header from "./components/Header";
+import { useState, useEffect } from "react";
 import Tasks from "./components/Tasks";
 import Footer from "./components/Footer";
+import ThemeContext from "./ThemeContext";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const buttonImgSources = {
+    light: "/images/icon-moon.svg",
+    dark: "/images/icon-sun.svg",
+  };
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
   return (
-    <section className="app">
-      <Header />
-      <Tasks />
-      <Footer />
-      <p className="tip">Drag and drop to reorder list</p>
-    </section>
+    <ThemeContext.Provider value={theme}>
+      <section className="app">
+        <div className="app__header flex flex-d-r flex-jc-sb flex-ai-b">
+          <h1>TODO</h1>
+          <button
+            onClick={() => {
+              if (theme == "dark") {
+                setTheme("light");
+              } else {
+                setTheme("dark");
+              }
+            }}
+          >
+            <img src={buttonImgSources[theme]} />
+          </button>
+        </div>
+
+        {/* <Tasks />
+        <Footer /> */}
+        <p className="tip">Drag and drop to reorder list</p>
+      </section>
+    </ThemeContext.Provider>
   );
 }
 
