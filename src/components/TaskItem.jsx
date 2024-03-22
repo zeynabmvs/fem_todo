516c8971cde8d04393e383558b8400c116fbd550
Zeynab Movasaqpoor <zeynab.mvs@gmail.com>
2024-03-23 00:16:12 +0330

    fix

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -5,21 +5,19 @@ export default function TaskItem({ todo, index }) {
   const { toggleCompleted, handleDelete } = useContext(ToDoContext);
 
   return (
-    <div
-      className={"task item " + (todo.completed ? "completed" : "active")}
-      
-    >
+    <li className={"task item " + (todo.completed ? "completed" : "active")}>
       <input
         className={"checkbox " + (todo.completed ? "completed" : "active")}
         type="checkbox"
         maxLength="200"
         onClick={() => toggleCompleted(todo.id)}
+        name="checkbox"
       ></input>
       <p>{todo.text}</p>
       <button
         className="delete_btn"
         onClick={() => handleDelete(todo.id)}
       ></button>
-    </div>
+    </li>
   );
 }
--- src/components/TasksList.jsx
+++ src/components/TasksList.jsx
@@ -2,19 +2,16 @@ import { useContext } from "react";
 import { ToDoContext, FilterContext } from "../contexts";
 import TaskItem from "./TaskItem";
 
-
 export default function TasksList() {
   const { currentFilter, filteredTodos } = useContext(FilterContext);
-  
+
   return (
-    <div className="app_tasks">
+    <ul className="app_tasks">
       {filteredTodos(currentFilter)
         .sort((a, b) => b.id - a.id)
         .map((todo, index) => {
-          return (
-            <TaskItem todo={todo} index={index} key={index}/>
-          );
+          return <TaskItem todo={todo} index={index} key={index} />;
         })}
-    </div>
+    </ul>
   );
 }
