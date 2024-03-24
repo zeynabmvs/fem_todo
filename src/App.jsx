e15676d78a6a711b393321d05561d0b628fe26d9
Zeynab Movasaqpoor <zeynab.mvs@gmail.com>
2024-03-24 19:58:09 +0330

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,37 @@ import { useState, useEffect } from "react";
 import Tasks from "./components/Tasks";
 import ThemeContext from "./contexts";
 import ToggleThemeBtn from "./components/ToggleThemeBtn";
-import useThemeToggle from "./hooks/useThemeToggle";
+
+function toggleTheme(theme) {
+  // create a temporarys stylesheet for no-transition
+  const css = document.createElement("style");
+  css.type = "text/css";
+  css.appendChild(
+    document.createTextNode(
+      `* {
+           -webkit-transition: none !important;
+           -moz-transition: none !important;
+           -o-transition: none !important;
+           -ms-transition: none !important;
+           transition: none !important;
+        }`
+    )
+  );
+  document.head.appendChild(css);
+
+  // Toggle theme
+  document.body.className = theme;
+
+  // Calling getComputedStyle forces the browser to redraw
+  const _ = window.getComputedStyle(css).opacity;
+  document.head.removeChild(css);
+}
 
 function App() {
   const [theme, setTheme] = useState("dark");
 
   useEffect(() => {
-    useThemeToggle(theme);
+    toggleTheme(theme);
   }, [theme]);
 
   return (
