2e7adc9282204861f2fe7ebe315c70f9840669fe
Zeynab Movasaqpoor <zeynab.mvs@gmail.com>
2024-03-27 10:14:03 +0330

    dark theme: add localStorage & preferColor support

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,18 @@
 import { useState, useEffect } from "react";
 import Tasks from "./components/Tasks";
-import ThemeContext from "./contexts";
 import ToggleThemeBtn from "./components/ToggleThemeBtn";
 
-function toggleTheme(theme) {
-  // create a temporarys stylesheet for no-transition
-  const css = document.createElement("style");
-  css.type = "text/css";
-  css.appendChild(
-    document.createTextNode(
-      `* {
-           -webkit-transition: none !important;
-           -moz-transition: none !important;
-           -o-transition: none !important;
-           -ms-transition: none !important;
-           transition: none !important;
-        }`
-    )
-  );
-  document.head.appendChild(css);
-
-  // Toggle theme
-  document.body.className = theme;
-
-  // Calling getComputedStyle forces the browser to redraw
-  window.getComputedStyle(css).opacity;
-  document.head.removeChild(css);
-}
-
 function App() {
-  const [theme, setTheme] = useState("dark");
-
-  useEffect(() => {
-    toggleTheme(theme);
-  }, [theme]);
-
+  
   return (
-    <ThemeContext.Provider value={{ theme, setTheme }}>
-      <section className="app">
-        <div className="app__header flex flex-d-r flex-jc-sb flex-ai-b">
-          <h1>TODO</h1>
-          <ToggleThemeBtn />
-        </div>
-        <Tasks />
-        <p className="tip">Drag and drop to reorder list</p>
-      </section>
-    </ThemeContext.Provider>
+    <section className="app">
+      <div className="app__header flex flex-d-r flex-jc-sb flex-ai-b">
+        <h1>TODO</h1>
+        <ToggleThemeBtn />
+      </div>
+      <Tasks />
+      <p className="tip">Drag and drop to reorder list</p>
+    </section>
   );
 }
 
--- src/components/ToggleThemeBtn.jsx
+++ src/components/ToggleThemeBtn.jsx
@@ -1,8 +1,90 @@
-import { useContext } from "react";
-import ThemeContext from "../contexts";
+import { useState, useEffect } from "react";
 
 export default function ToggleThemeBtn() {
-  const { theme, setTheme } = useContext(ThemeContext);
+
+  function getInitialTheme () {
+    const storageTheme = localStorage.getItem("theme");
+
+    let currentTheme;
+
+    if (storageTheme) {
+      currentTheme = storageTheme;
+    } else {
+      const darkModeQuery = window.matchMedia("(prefers-color-scheme: dark)");
+      currentTheme = darkModeQuery.matches ? "dark" : "light";
+    }
+
+    return currentTheme
+  }
+
+  const [theme, setTheme] = useState(getInitialTheme);
+
+  function toggleTheme() {
+    // Toggle theme
+    if (theme === "dark") {
+      localStorage.setItem("theme", "light");
+      setTheme("light");
+    } else {
+      localStorage.setItem("theme", "dark");
+      setTheme("dark");
+    }
+  }
+
+  // function handleToggleBtn() {
+  //   // create a temporarys stylesheet for no-transition
+  //   const css = document.createElement("style");
+  //   css.type = "text/css";
+  //   css.appendChild(
+  //     document.createTextNode(
+  //       `* {
+  //            -webkit-transition: none !important;
+  //            -moz-transition: none !important;
+  //            -o-transition: none !important;
+  //            -ms-transition: none !important;
+  //            transition: none !important;
+  //         }`
+  //     )
+  //   );
+  //   document.head.appendChild(css);
+
+  //   toggleTheme();
+
+  //   // Calling getComputedStyle forces the browser to redraw
+  //   window.getComputedStyle(css).opacity;
+  //   document.head.removeChild(css);
+  // }
+
+  useEffect(() => {
+    // const storageTheme = localStorage.getItem("theme");
+    const darkModeQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+    // let currentTheme;
+
+    // if (storageTheme) {
+    //   currentTheme = storageTheme;
+    // } else {
+    //   currentTheme = darkModeQuery.matches ? "dark" : "light";
+    // }
+
+    // document.body.className = currentTheme;
+    // setTheme(currentTheme);
+
+    function handleColorPrefer(e) {
+      e.matches ? setTheme("dark") : setTheme("light");
+    }
+
+    // listening for changes in prefer color scheme
+    darkModeQuery.addEventListener("change", handleColorPrefer);
+
+    // Clean up the listener
+    return () => {
+      darkModeQuery.removeEventListener("change", handleColorPrefer);
+    };
+  }, []);
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
 
   const buttonImgSources = {
     light: "/images/icon-moon.svg",
@@ -10,12 +92,7 @@ export default function ToggleThemeBtn() {
   };
 
   return (
-    <button
-      onClick={() => {
-        theme == "dark" ? setTheme("light") : setTheme("dark");
-      }}
-      title={theme}
-    >
+    <button onClick={toggleTheme} title="Toggles light & dark">
       <img src={buttonImgSources[theme]} />
     </button>
   );
